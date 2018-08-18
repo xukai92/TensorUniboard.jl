8f756863ce53713c1f1ac6636023df3adc30722b
Kai Xu <xukai921110@gmail.com>
2018-08-18 14:12:40 +0100

    implement plot for LineDataGroup

--- demo.jl
+++ demo.jl
@@ -1,5 +1,5 @@
 using UnicodePlots
-import Base: push!, getindex
+import Base: push!, getindex, length
 
 clean_line_and_cursor_up(n) = (for _ = 1:n print("\r\e[K\e[A") end)
 
@@ -22,6 +22,8 @@ struct LineDataGroup
   line_data_arr::Vector{LineData}
 end
 getindex(ldg::LineDataGroup, idx::Int) = ldg.line_data_arr[idx]
+length(ldg::LineDataGroup) = length(ldg.line_data_arr)
+
 get_x_mat(ldg::LineDataGroup) = cat(map(ld -> ld.x, ldg.line_data_arr)..., dims=[2])
 get_y_mat(ldg::LineDataGroup) = cat(map(ld -> ld.y, ldg.line_data_arr)..., dims=[2])
 
@@ -38,6 +40,18 @@ function get_ylim(ldg::LineDataGroup)
   return [y_min, y_max]
 end
 
+function plot(ldg::LineDataGroup, title::AbstractString)
+  # TODO: implement ignore extreme
+  p = lineplot(ldg[1].x, ldg[1].y, name=ldg[1].name,
+               xlim=get_xlim(ldg), ylim=get_ylim(ldg),
+               grid=false, width=60, height=25)
+  for i = 2:length(ldg)
+    lineplot!(p, ldg[i].x, ldg[i].y, name=ldg[i].name)
+  end
+  title!(p, title)
+  return p
+end
+
 ld1 = LineData("sin")
 ld2 = LineData("cos")
 ldg = LineDataGroup([ld1, ld2])
@@ -45,12 +59,7 @@ ldg = LineDataGroup([ld1, ld2])
 for i = 1:length(data_x)
   push!(ldg[1], data_x[i], data_y1[i])
   push!(ldg[2], data_x[i], data_y2[i])
- # TODO: implement ignore extreme
-  p = lineplot(ld1.x, ld1.y, name=ld1.name,
-               xlim=get_xlim(ldg), ylim=get_ylim(ldg),
-               grid=false, width=60, height=25)
-  lineplot!(p, ld2.x, ld2.y, name=ld2.name)
-  title!(p, "demo")
+  p = plot(ldg, "demo")
   show(p)
   p_str = string(p)
   # TODO: find a more efficient way to get the plot height
