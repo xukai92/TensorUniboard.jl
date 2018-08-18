a48e34acd3e5e56b9636935fccb9f28587262282
Kai Xu <xukai921110@gmail.com>
2018-08-18 03:05:50 +0100

    use struct to handle line data

--- demo.jl
+++ demo.jl
@@ -1,4 +1,5 @@
 using UnicodePlots
+import Base: push!
 
 clean_line_and_cursor_up(n) = (for _ = 1:n print("\r\e[K\e[A") end)
 
@@ -9,27 +10,30 @@ data_y1 = sin.(data_x)
 data_y2 = cos.(data_x)
 
 # Simulate data stream
-x1 = Float64[]
-y1 = Float64[]
-x2 = Float64[]
-y2 = Float64[]
+struct LineData
+  x::Vector{Float64}
+  y::Vector{Float64}
+  name::String
+end
+LineData(name) = LineData(Float64[], Float64[], name)
+push!(ld::LineData, x::Float64, y::Float64) = (push!(ld.x, x); push!(ld.y, y))
+
+ld1 = LineData("sin")
+ld2 = LineData("cos")
 
 for i = 1:length(data_x)
-    push!(x1, data_x[i])
-    push!(y1, data_y1[i])
-    push!(x2, data_x[i])
-    push!(y2, data_y2[i])
-    # TODO: implement ignore extreme
-    p = lineplot(x1, y1,
-                 name="sin", grid=false,
-                 width=60, height=25)
-    lineplot!(p, x2, y2, name="cos")
-    title!(p, "demo")
-    show(p)
-    p_str = string(p)
-    # TODO: find a more efficient way to get the plot height
-    p_height = length(split(p_str, "\n")) - 1
-    #  p_height = nrows(p.graphics)  # this should be fast but it doesn't include rows for border, title, etc
-    sleep(0.5)
-    clean_line_and_cursor_up(p_height)
+  push!(ld1, data_x[i], data_y1[i])
+  push!(ld2, data_x[i], data_y2[i])
+  # TODO: implement ignore extreme
+  p = lineplot(ld1.x, ld1.y, name=ld1.name,
+               grid=false, width=60, height=25)
+  lineplot!(p, ld2.x, ld2.y, name=ld2.name)
+  title!(p, "demo")
+  show(p)
+  p_str = string(p)
+  # TODO: find a more efficient way to get the plot height
+  p_height = length(split(p_str, "\n")) - 1
+  #  p_height = nrows(p.graphics)  # this should be fast but it doesn't include rows for border, title, etc
+  sleep(0.5)
+  clean_line_and_cursor_up(p_height)
 end
