df3ff0280cf2a51fbf9bbf1b2090aea9960274d8
Kai Xu <xukai921110@gmail.com>
2018-08-18 02:56:50 +0100

    demo with two lines

--- demo.jl
+++ demo.jl
@@ -5,23 +5,31 @@ clean_line_and_cursor_up(n) = (for _ = 1:n print("\r\e[K\e[A") end)
 x_start = 0
 x_end = 5
 data_x = 0:0.1:5
-data_y = sin.(data_x)
+data_y1 = sin.(data_x)
+data_y2 = cos.(data_x)
 
 # Simulate data stream
-x = Float64[]
-y = Float64[]
+x1 = Float64[]
+y1 = Float64[]
+x2 = Float64[]
+y2 = Float64[]
 
 for i = 1:length(data_x)
-    push!(x, data_x[i])
-    push!(y, data_y[i])
-    p = lineplot(x, y,
-                 title="demo", name="sin", grid=false,
-                 #  xlim=[x_start, x_end], ylim=[-1, 1], # TODO: implement ignore extreme
+    push!(x1, data_x[i])
+    push!(y1, data_y1[i])
+    push!(x2, data_x[i])
+    push!(y2, data_y2[i])
+    # TODO: implement ignore extreme
+    p = lineplot(x1, y1,
+                 name="sin", grid=false,
                  width=60, height=25)
+    lineplot!(p, x2, y2, name="cos")
+    title!(p, "demo")
     show(p)
     p_str = string(p)
     # TODO: find a more efficient way to get the plot height
     p_height = length(split(p_str, "\n")) - 1
+    #  p_height = nrows(p.graphics)  # this should be fast but it doesn't include rows for border, title, etc
     sleep(0.5)
     clean_line_and_cursor_up(p_height)
 end
